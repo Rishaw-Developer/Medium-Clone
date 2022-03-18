46d40cbfdaeb25d58a7b91b327af52f5da077aa0
Rishaw <ratneshthakur.40@gmail.com>
2022-03-18 21:46:19 +0530

    Added SSR and posts section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typing'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+const Home = ({ posts }: Props) => {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
       </Head>
       <Header />
-      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+      <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-0">
         <div className="space-y-5 px-10">
           <h1 className="max-w-xl font-serif text-6xl">
             <span className="underline decoration-black decoration-4">
@@ -23,13 +30,59 @@ const Home: NextPage = () => {
           </h2>
         </div>
         <img
-          className="hidden md:inline-flex h-32 lg:h-full"
+          className="hidden h-32 md:inline-flex lg:h-full"
           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt=""
         />
       </div>
+
+      {/* Posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-1 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts?.map((post) => (
+          <Link key={post._id} href={`/post/${post?.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-sm">{post.description}</p>
+                  <p className="text-xs">By {post.author.name}</p>
+                </div>
+                <Link href={`/authors/${post.author.slug.current}`}>
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                    alt=""
+                  />
+                </Link>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name, image, slug
+  },description,
+  mainImage,
+  slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+  return {
+    props: { posts },
+  }
 }
 
 export default Home
